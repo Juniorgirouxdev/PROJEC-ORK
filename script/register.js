d4ee6055a306e7ea1a5e32a2f2fae53c0cf5b890
Eduardo3787 <magnoeduardo2019@gmail.com>
2023-10-17 16:00:22 -0300

    teste para desabilitar o botao

--- script/register.js
+++ script/register.js
@@ -1,12 +1,17 @@
+
+
 document
   .getElementById("formulario")
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
-    let repetir = document.getElementById("repetir").value;
+    const name = document.getElementById("name").value;
+   const email = document.getElementById("email").value;
+   const senha = document.getElementById("senha").value;
+   const repetir = document.getElementById("repetir").value;
+
+
+    
 
     if (!name || !email || !senha || !repetir) {
       document.getElementById('nameError').textContent =
@@ -16,4 +21,29 @@ document
       document.getElementById('nameError').textContent = "";
     }
   });
+
+  document.getElementById('name').addEventListener('input', botaoDesabilitado)
+  email.addEventListener('input', botaoDesabilitado)
+  senha.addEventListener('input', botaoDesabilitado)
+  repetir.addEventListener('input', botaoDesabilitado)
+
+  let btn = document.getElementById('botao')
+function botaoDesabilitado () {
+
+     
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    let repetir = document.getElementById("repetir").value;
+
+    if(  name && email && senha && repetir){
+
+        btn.removeAttribute('disabled')
+
+    }
+    
+    else{
+        btn.setAttribute('disabled', 'true')
+    }
+}
   
